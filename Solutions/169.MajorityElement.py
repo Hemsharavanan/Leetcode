7282cfbddf86cc8d874f69dd22948cde69e0a24a
Hemsharavanan <hemsharavanan@gmail.com>
2021-05-16 21:31:13 +0530

    169.Majority Element

---
+++ Solutions/169.MajorityElement.py
@@ -0,0 +1,37 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        treshold = len(nums) // 2
+        
+        elementsCount = {}
+        
+        for num in nums:
+            elementsCount[num] = elementsCount.get(num, 0) + 1
+        
+        for key, value in elementsCount.items():
+            if value > treshold:
+                return key
+
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-231 <= nums[i] <= 231 - 1
+"""
